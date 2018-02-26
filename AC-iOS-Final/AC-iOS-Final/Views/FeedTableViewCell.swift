f736a2e72aa56783a6f9e5dd1777b10dd152359a
Maryann Yin <maryannyin@ac.c4q.nyc>
2018-02-26 11:32:22 -0500

    working on the feed view

--- AC-iOS-Final/AC-iOS-Final/Views/FeedTableViewCell.swift
+++ AC-iOS-Final/AC-iOS-Final/Views/FeedTableViewCell.swift
@@ -9,16 +9,49 @@
 import UIKit
 
 class FeedTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    lazy var dataContainer: UIView = {
+        let dc = UIView()
+        dc.backgroundColor = .white
+        return dc
+    }()
+    
+    lazy var commentTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = UIColor.yellow
+        textView.textColor = UIColor.black
+        textView.isEditable = true
+        textView.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.medium)
+        return textView
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: "CategoryCell")
+        setupViews()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setupViews() {
+    }
+    
+    private func setupDataContainer() {
+        addSubview(dataContainer)
+        dataContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [dataContainer.centerXAnchor.constraint(equalTo: centerXAnchor),
+             dataContainer.centerYAnchor.constraint(equalTo: centerYAnchor)])
+    }
+    
+    private func setupCommentTextView: UITextView = {
+        addSubview(commentTextView)
+        commentTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [commentTextView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            commentTextView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            commentTextView.topAnchor.constraint(equalTo: dataContainer.backgroundColor, constant: 2)])
     }
 
 }
